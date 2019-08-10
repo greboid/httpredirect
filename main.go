a313c7a0711afa764a3fbd762b80387a0a469586
Greg Holmes <git@greg.holmes.name>
2019-08-10 20:53:30 +0100

    Add some variables to allow configuration.
    Closes #1
    Closes #2
    Closes #3

--- main.go
+++ main.go
@@ -6,24 +6,43 @@ import (
 	"os"
 )
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
+var targetHost, targetHostError = os.LookupEnv("TARGETHOST")
+var excludeQuery, excludeQueryError = os.LookupEnv("EXCLUDEQUERY")
+var targetPath, pathError = os.LookupEnv("TARGETPATH")
+var targethttp, targethttpError = os.LookupEnv("TARGETHTTP")
 
 func redirect(w http.ResponseWriter, req *http.Request) {
-	target := "https://" + getEnv("TARGETHOST", req.Host) + req.URL.Path
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
+	var target string
+	//Check if http or https
+	if !targethttpError {
+		target += "https://"
+	} else {
+		target += "http://"
+	}
+	//Check what host to point at, defaulting to the requested host
+	if !targetHostError {
+		target += req.Host
+	} else {
+		target += targetHost
+	}
+	//Check whether to redirect to a fixed path, or existing
+	if !pathError {
+		target += req.URL.Path
+	} else {
+		target += targetPath
+	}
+	//Should we include the query parameters?
+	if !excludeQueryError {
+		if len(req.URL.RawQuery) > 0 {
+			target += "?" + req.URL.RawQuery
+		}
 	}
 	log.Printf("redirect to: %s", target)
 	http.Redirect(w, req, target, http.StatusPermanentRedirect)
 }
 
 func main() {
-	if err := http.ListenAndServe(":80", http.HandlerFunc(redirect)); err != http.ErrServerClosed {
+	if err := http.ListenAndServe(":1234", http.HandlerFunc(redirect)); err != http.ErrServerClosed {
 		log.Fatalf("ListenAndServe(): %s", err)
 	}
 }
